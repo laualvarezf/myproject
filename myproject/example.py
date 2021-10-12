eafddf1ba3145a448d4d0a7e8195c722dab4ce36
Laura Alvarez <lauraalvarezfuentes@gmail.com>
2021-10-12 18:22:39 +0100

    first push

---
+++ myproject/example.py
@@ -0,0 +1,12 @@
+def add_one(number):
+    return number + 1
+
+def salute(name):
+    return f'Hello {name} my name is Laura and it is a pleasure to meet you 😁'
+
+
+if __name__ == "__main__":
+    # Le Wagon location
+    number = 10
+    result = add_one(number)
+    print(result)
